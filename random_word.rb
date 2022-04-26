7a09ef8d2df3957ea02d1b8ba5502d920a617dd3
Phuong Thanh <89241218+phuongThanhKMA@users.noreply.github.com>
2022-04-26 09:54:08 +0700

    Add files via upload

---
+++ random_word.rb
@@ -0,0 +1,14 @@
+class RandomWord
+  def initialize
+    #  print("Enter a dictionary file name: ")
+    #  filename = gets.chomp()  
+    filename = File.open('GK\dictionary.txt')
+    word_list = File.readlines(filename)
+    @index = word_list.shuffle!
+
+  end
+
+  def pick_random_word
+    @words=@index.sample
+  end
+end
