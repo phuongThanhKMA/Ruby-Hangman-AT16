5e6ffe79c4f93e24c4284f3d8f29e981fa20fea2
Phuong Thanh <89241218+phuongThanhKMA@users.noreply.github.com>
2022-04-28 07:33:50 +0700

    Update finnally

--- display.rb
+++ display.rb
@@ -1,53 +1,6 @@
 class Display
-  require 'json'
+
   def initialize()
-    @player_name = ""
-    @saved_game = ""
-  end
-
-  def to_json
-    JSON.generate({player_name: @player_name, point: @point})
-  end
-
-  def save_game(string) 
-    File.open("saved.json", "a") do |game_file|
-      game_file.write(string)
-      end
-    puts "Game saved!"
-  end
-
-  def winner
-    print %Q(
-        You've guessed the word correctly. You are a winner!
-    )
-    print %Q(
-     What's your name?
-  )
-    @player_name = gets.chomp
-    print %Q(
-      Do you wanna save your point?
-      Press 1 if you want.
-    )
-    @saved_game = gets.chomp
-    case @saved_game
-    when "1"
-      @point = $lives
-      save_game(to_json)
-    else
-      print %Q(
-        Goodbye #{@player_name} Thanks for playing Hangman.
-      ) 
-    end
-
-  end
-
-
-  def loser
-    print %Q(
-        You have run out of lives. You're dead!
-        You're a loser.
-        The game is over.
-    )
   end
 
   def game_rules
@@ -56,6 +9,20 @@ class Display
         To win, you need to guess the mystery word or you die.
         You can have up to 8 incorrect guesses, before you're hanged.
         Let's begin!
+    )
+  end
+
+  def winner
+    print %Q(
+        You've guessed the word correctly. You are a winner!
+    )
+  end
+
+  def loser
+    print %Q(
+        You have run out of lives. You're dead!
+        You're a loser.
+        The game is over.
     )
   end
 
@@ -140,7 +107,7 @@ class Display
       =======)
     when 0
       print """
-      +-----++
+      +-----+
       |     |
       0     |
      /|\\    |
